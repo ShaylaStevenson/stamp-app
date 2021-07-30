f4679618fd044125f2cac94b37904346dc6055ad
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-07-30 15:37:48 -0700

    Updates

---
+++ precache-manifest.59a517c8746aa9ffbe66b6399cee6317.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3992743b6541581f62a06f4ec70bb637",
+    "url": "/stamp-app/index.html"
+  },
+  {
+    "revision": "42e2fe2ed9da79d7923c",
+    "url": "/stamp-app/static/css/main.879b3096.chunk.css"
+  },
+  {
+    "revision": "abf87b5ce0734232411d",
+    "url": "/stamp-app/static/js/2.40b3acd0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/stamp-app/static/js/2.40b3acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e2fe2ed9da79d7923c",
+    "url": "/stamp-app/static/js/main.7c443d59.chunk.js"
+  },
+  {
+    "revision": "e6abedcadb958d280903",
+    "url": "/stamp-app/static/js/runtime-main.5c809991.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/stamp-app/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "bd0f25302677410d00e0a550fc54e8f3",
+    "url": "/stamp-app/static/media/purpleFlower.bd0f2530.jpg"
+  }
+]);
